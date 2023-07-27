5aff38e8e0cbfba4244ae15edfd5066e84c3c143
Mercury Evans <m@rcury.com>
2023-07-27 11:24:03 +0100

    Initial Commit

---
+++ MegatenFusionTool/MegatenFusionToolApp.swift
@@ -0,0 +1,20 @@
+//
+//  MegatenFusionToolApp.swift
+//  MegatenFusionTool
+//
+//  Created by Mercury Evans on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MegatenFusionToolApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
